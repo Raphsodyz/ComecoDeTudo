13398a06518f00694d317e1530db8c47454acc77
John DoeRaphsodyz <arthurvsr@gmail.com>
2021-12-29 20:18:42 -0300

    Adições no modal

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,10 +1,10 @@
-// $('selectHamburguer').click(() => {
-//     let itemSelecionado = {};
-//     sanduiche.nome = $('#sanduicheTipo').val();
-// })
-
 window.onload = function (){
     
     var sanduicheModalTitle = $('#sanduicheTipo1').text();
     document.getElementById('itemSelecionado').innerHTML = sanduicheModalTitle;
+    var sanduicheModalBody = document.querySelector(".modal-body");
+    var sanduicheModalImage = document.createElement("IMG");
+    sanduicheModalImage.width = "450";
+    sanduicheModalImage.src = document.getElementById("img-hamburguer").src;
+    sanduicheModalBody.appendChild(sanduicheModalImage);
 }

58259d45e789dc0222be37c5e87fd133d17c4112
John DoeRaphsodyz <arthurvsr@gmail.com>
2022-01-05 00:15:54 -0300

    Adições modal

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,13 +1,13 @@
 var sanduicheModalAddition = document.getElementsByClassName('form-check form-check-inline');
 
-window.onload = function (){
-    
-    var sanduicheModalTitle = $('#sanduicheTipo1').text();
-    document.getElementById('itemSelecionado').innerHTML = sanduicheModalTitle;
+function selecionarSanduiche(idSanduiche, idImage){
+
+    var sanduicheSelecionado = $('#sanduicheTipo' + idSanduiche).text();
+    document.getElementById('itemSelecionado').innerHTML = sanduicheSelecionado;
     var sanduicheModalBody = document.querySelector(".modal-body");
-    var sanduicheModalImage = document.createElement("IMG");
+    var sanduicheModalImage = document.getElementById('sanduicheModalImage');
     sanduicheModalImage.width = "450";
-    sanduicheModalImage.src = document.getElementById("img-hamburguer1").src;
+    sanduicheModalImage.src = document.getElementById("img-hamburguer" + idImage).src;
     sanduicheModalBody.appendChild(sanduicheModalImage);
 }
 
@@ -20,22 +20,23 @@ function displayAddition(){
             sanduicheModalAddition[i].style.display = "flex";
         }
     } else {
-        for(var i = 0; i < sanduicheModalAddition.length; i++){
-            sanduicheModalAddition[i].style.display = "none";
+        for(var j = 0; j < sanduicheModalAddition.length; j++){
+            sanduicheModalAddition[j].style.display = "none";
         }
     }
 }
 
 function modalClear(){
+
     document.getElementById('quantidade').value = '';
     document.getElementById('modalObsText').value = '';
     var sanduicheModalAdditionClear = document.getElementsByClassName('form-check-input');
-    for(var j = 0; j < sanduicheModalAdditionClear.length; j++){
-        sanduicheModalAdditionClear[j].checked = false;
+    for(var k = 0; k < sanduicheModalAdditionClear.length; k++){
+        sanduicheModalAdditionClear[k].checked = false;
     }
     
-    for(var k = 0; k < sanduicheModalAddition.length; k++){
-        sanduicheModalAddition[k].style.display = "none";
+    for(var l = 0; l < sanduicheModalAddition.length; l++){
+        sanduicheModalAddition[l].style.display = "none";
     }
 
 }
